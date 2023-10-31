81ff745d73a806ad065b255f465c55ba407bc3c2
kaka0913 <kabuyuu0913@gmail.com>
2023-11-01 03:41:28 +0900

    ~34

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// domain
+import 'package:udemy_flutter_sns/domain/firestore_user.dart';
  
 final mainProvider = ChangeNotifierProvider(
   (ref) => MainModel()
@@ -14,12 +16,16 @@ class MainModel extends ChangeNotifier {
   int counter = 0;
   Future<void> createUser({required BuildContext context}) async {
     final String v4 = const Uuid().v4();
-    final Map<String,dynamic> userData = {
-      "userName": "Alice",
-      "uid": v4,
+    final FirestoreUser firestoreUser = FirestoreUser(uid: v4,userName: "Alice");
+    final Map<String,dynamic> userData = firestoreUser.toJson();
+    final Map<String,dynamic> userData2 = {
+      "uid": "abcd",
+      "userName": "Bob",
     };
+    final FirestoreUser firestoreUser2 = FirestoreUser.fromJson(userData2);
+    debugPrint(firestoreUser2.userName);
     await FirebaseFirestore.instance.collection("users").doc().set(userData);
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('ユーザーが作成できました')));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
     notifyListeners();
   }
   
