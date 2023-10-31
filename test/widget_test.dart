8c811e957923253d4052f50f2d489e110c9eb90c
kaka0913 <kabuyuu0913@gmail.com>
2023-11-01 02:21:41 +0900

    ~13

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:udemy_flutter_sns/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(title: '',));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
